c5d8f4d4c22079b0bb99181f33d77ebf8ffe91a1
ngothinhphat <ngothinhphat244@gmail.com>
2024-11-02 16:45:11 +0700

    fix -v3

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+
 const UserSchema = new Schema({
     username: {
         type: String,
